33db63e74698c1b5d8c5d27ffbc8dcf9295072a8
Lem Canady <lem.canady@gmail.com>
2019-01-17 10:37:49 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const Formatter = require('mu-format')
+
+const formatter = new Formatter({})
+
+const app = express()
+const port = 3000
+
+app.use(function(err, req, res, next){
+  if(err) {
+    res.status(500).json({error:err})
+  }
+})
+
+
+app.get('/', async (req, res) => {
+
+  // If a request is sent with text, process.
+  if (req.query.txt){
+    let txt = req.query.txt
+    if (txt.match(/github.*/i)){
+      txt = txt.replace(/\s/,'')
+    }
+    try {
+      await formatter.format(txt)
+      formatter.once('done', documents => {
+        res.json({documents})
+      })   
+      formatter.on('log', log => console.log(log))
+      formatter.on('error', error => res.status(500).json({error}))
+    } catch (error) {
+      res.status(500).json({error})
+    }
+  } else {
+    res.status(500).json({error: 'Txt query parameter required.'})
+  }
+
+
+
+
+
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
