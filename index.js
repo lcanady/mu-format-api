d659b9f41f0d9ecc0939249cd8a892074da46f85
Lem Canady <lem.canady@gmail.com>
2019-01-20 02:47:06 -0800

    api updates

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const Formatter = require('mu-format')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
-const formatter = new Formatter()
 const PORT = process.env.PORT || 3001;
 const app = express()
 
 // Use Node.js body parsing middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(cors())
 
 
 app.get('/', (req, res) => {
@@ -17,6 +17,11 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', async (req,res) => {
+  
+  const formatter = new Formatter({
+    headers: 'system author email url'
+  })
+
   // if a URL is provided.
   if (req.body.url) {
     try {
@@ -25,7 +30,7 @@ app.post('/', async (req,res) => {
       res.send({error: error.message})
     }
     formatter.once('done', docs => res.send({documents: docs}))
-    formatter.on('error', error => res.send({error: error.message}))  
+    formatter.once('error', error => res.send({error: error.message}))  
   // Else just parse the text.
   } else {
     if (req.body.txt){
